e06da266d5eacb47ca30b4a2b1ae222fc54c58ef
Sandeep Srinivasa <sss@tradus.com>
2012-09-13 16:01:01 +0530

    working sessions with Rack::Session::Pool

--- app.rb
+++ app.rb
@@ -2,26 +2,66 @@ require 'rubygems'
 require 'sinatra'
 require 'uuidtools'
 require 'rack/session/pool'
+#require 'rack/protection'
+#require 'rack/protection/authenticity_token'
+require 'securerandom'
+require 'slim'
+
+=begin
+%w(models controllers concerns).each do |name|
+   Dir[File.join('app', name, '**/*.rb')].each do |file|
+     require_relative file
+   end
+ end
+=end
+
+
+class SSSAuthenticityToken < Rack::Protection::AuthenticityToken
+  def accepts?(env)
+    return true if safe?(env)
+    super(env)
+  end
+end
 
 class App < Sinatra::Base
+  enable :logging
+#configure do
+    #use ::Rack::Protection::AuthenticityToken, {}
+    set :protection, :except => [:authenticity_token, :form_token, :json_csrf, :remote_referrer, :form_token, :escaped_params, :frame_options, :path_traversal, :session_hijacking, :ip_spoofing]
+    #use Rack::Protection::FormToken
+    #disable :protection
+    #use ::Rack::MethodOverride
+    #use Rack::Session::Cookie, :secret => "sss" 
+    #use ::Rack::Session::Pool, :key => "sss", :secret => "dfjd;fj;dfj;df;ldfl;df;", :expire_after => 2592000 
+    #use ::Rack::Protection::AuthenticityToken
+    #use SSSAuthenticityToken
+    #enable :sessions, :logging
+#end
 
-  use Rack::Session::Pool, :expire_after => 2592000, :user_id => "sss"
+#  use Rack::Session::Pool
   #use Rack::Session::PersistentSessions, 'sqlite://blog.db'
   #use Rack::Session::Persistent
   #enable :sessions
 
+
+  #set :protection
+
   def call(env)
     #global logging
     puts "startup = #{settings.time_at_startup} vs #{Time.now}"
-    puts 'manager: ' + env['REQUEST_METHOD'] + ' ' + env['REQUEST_URI']
+    puts 'manager: ' + env['REQUEST_METHOD'] + ' ' + env['REQUEST_URI'] + " session_key = #{settings.session_key}"
     #super calls the "get", "post", etc. functions
     status, headers, body = super
+
+    #session = session env
+    #puts "session key = #{session[:key]}"
+
     headers['X-Middleware'] = 'true'
-    puts "d - > #{headers}"
     Rack::Utils.set_cookie_header! headers, :last_access,  {:value => Time.now.utc,:expires => (Time.now + 52*7*24*60*60) }
+
     [status, headers, body]
   end
-  
+
   configure :development do
     #also_reload 'helpers/*.rb'
     #also_reload 'models/*.rb'
@@ -33,17 +73,29 @@ class App < Sinatra::Base
     def cache_for(time)
       response.headers['Cache-Control'] = "public, max-age=#{time.to_i}"
     end
+
+    def csrf_token
+      session[:csrf] ||= SecureRandom.hex 32
+    end
+  end
+
+  post "/submit/" do
+    response.body = "success"
+    response.finish
   end
 
   get "/" do
     cache_for 10*80
-    session[:session_id] = UUIDTools::UUID.timestamp_create.to_s
+    #session[:session_id] = UUIDTools::UUID.timestamp_create.to_s
+    
 
     response.set_cookie "user_id", {:value => "sss", 
                                     :expires => (Time.now + 52*7*24*60*60)}
     response.headers['X-SSS'] = "SSS"
-    response.body = "SSS"
-    response.finish
+    #response.body = "SSS"
+    puts "session = #{@pool.inspect} oor #{session.inspect} and #{session.to_hash.inspect} key =#{session[:session_id]}"
+    slim :index
+    # response.finish only if you are not using a template rendering call above
   end
 
 end
--- config.ru
+++ config.ru
@@ -1,4 +1,17 @@
+#require 'rack/protection'
+#use Rack::Protection, :except => :xss_header
+#use Rack::Protection::AuthenticityToken
+require 'bundler'
+Bundler.require
+
+require 'sinatra'
+require 'rack'
+require 'rack/session/pool'
+
 require ::File.join( ::File.dirname(__FILE__), 'app' )
+lib_path = File.expand_path('../lib', __FILE__)
+$:.unshift(lib_path) unless $:.include?(lib_path)
+$:.unshift('.') 
 #puts "running in #{env} mode"
 
 #App.reset! if development?
@@ -6,7 +19,7 @@ require ::File.join( ::File.dirname(__FILE__), 'app' )
 #However, identical Sinatra routes do NOT override each other. Rather, the first route that is evaluated is used (more precisely, all routes appended to a list and the first matching one is used, so additional identical routes are never run).
 #Clearly, Rack::Reloader is not very useful if you can’t change the contents of any route. The solution is to throw away the old routes when the file is reloaded using  Reset!
 #This will be done from inside the app 
-use Rack::Reloader if development?
+use Rack::Reloader 
 
 
 incrementor = lambda do |env|
@@ -15,9 +28,31 @@ incrementor = lambda do |env|
 end
 
 App.set :time_at_startup, Time.now
-myapp = App.new
-sessioned = Rack::Session::Pool.new(myapp,
-    :expire_after => 2592000
-  )
-run sessioned
-#run myapp 
+App.set :session_key, "sss" 
+#set :session_key, "sss" 
+App.set :session_secret, "fflgjfljglfkjglfjg" 
+#set :session_secret, "fflgjfljglfkjglfjg" 
+
+=begin
+  myapp = App.new
+  sessioned = Rack::Session::Pool.new(myapp,
+      :key => "sss",
+      :secret => "dfjd;fj;dfj;df;ldfl;df;",
+      :expire_after => 2592000
+    )
+  run sessioned
+=end
+
+#prot = Rack::Protection.new(sessioned,{:except => [:xss_header]})
+#run sessioned 
+
+b = Rack::Builder.new do
+   use  ::Rack::Session::Pool, :key => "sss", :secret => "dfkjdlfjldjfldjkkn909", :expire_after => 2592000
+   #use ::Rack::Protection::AuthenticityToken
+   use ::Rack::MethodOverride
+
+  run App
+end.to_app
+
+#run App 
+run b
